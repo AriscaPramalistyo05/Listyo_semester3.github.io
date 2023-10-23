3be9eb617f3a1792d50853c2b7b079fc33b40e88
Listyo <Email>
2023-10-23 08:53:46 +0700

    change

---
+++ modul_3/Praktikum3_Arisca-Pramalistyo.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>belajar pemrograman web</title>
+
+</head>
+<body>
+    <h1>Selamat datang dan selamat belajar </h1>
+    <h2>Pada hari senin ini kita akan memulai dari dasar html</h2>
+    <p>HTML adalah bahasa pemrograman yang digunakan untuk membuat sebuah website.</p>
+    <p><b>Ada beberapa tag yang sering digunakan : </b></p>
+    <img src="tag html.webp" alt="" srcset="" height="600" >
+  
+
+    
+</body>
+</html>
